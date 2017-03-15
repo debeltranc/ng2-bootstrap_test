266019a687f9e260f677c4d346af4b5fa02bbd9f
David <debeltranc@unal.edu.co>
2017-03-15 08:02:17 +0000

    Added Boostrap Carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { CarouselModule } from 'ng2-bootstrap/carousel';
+
 import { AppComponent } from './app.component';
+import { Carousel } from './carousel.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations:  [
+    AppComponent,
+    Carousel
   ],
   imports: [
+    CarouselModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:[AppComponent]
 })
 export class AppModule { }
